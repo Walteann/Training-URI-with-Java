61cdaeb239474eb14288030841206964456e6d08
Walteann <walteann3@gmail.com>
2017-02-09 11:03:22 -0300

    cmmt 1059uri

---
+++ URI Problem/src/beginner/Exer1059.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        for(int i = 2; i <=100; i+=2){
+			System.out.println(i);
+		}
+
+ 
+    }
+ 
+}
